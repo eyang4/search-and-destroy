f7db5011f5cf0a3b5ddbe1b381c60acce9bbde1a
Eric Yang <yangeric12@gmail.com>
2019-12-11 16:38:03 -0500

    finished two variations of isLoop

--- looper/isloop.js
+++ looper/isloop.js
@@ -2,7 +2,28 @@
 
 //Complete this algo
 const isLoop = (linkedlist) => {
-
+  // if (linkedlist.tail.next) return true;
+  // const tracking = {};
+  // let currentNode = linkedlist.head;
+  // while(currentNode) {
+  //   if (tracking[currentNode.value]) return true; // looping
+  //   tracking[currentNode.value] = 'blah';
+  //   currentNode = currentNode.next;
+  // }
+  // return false;
+  if (!linkedlist.head.next) return false // single node, non circular
+  let slow = linkedlist.head
+  let fast = linkedlist.head
+  slow = slow.next
+  fast = fast.next.next
+  while(slow !== fast) {
+    slow = slow.next
+    if (!slow) return false // the following node is null, non circular
+    if (!fast.next) return false // make sure it is not null itself
+    fast = fast.next.next
+    if (!fast) return false // the following, following node is null, non circular
+  }
+  return true;
 };
 
 
